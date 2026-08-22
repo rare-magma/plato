662c45624180c2f99d1c6a5d2c4b8883a89d155e
rare-magma <rare-magma@posteo.eu>
2026-08-22 12:13:16 +0200

    fix: use 0 decimal cases in slider

--- crates/core/src/view/slider.rs
+++ crates/core/src/view/slider.rs
@@ -1,12 +1,14 @@
-use crate::device::CURRENT_DEVICE;
-use crate::unit::scale_by_dpi;
-use crate::framebuffer::{Framebuffer, UpdateMode};
-use crate::input::{DeviceEvent, FingerStatus};
-use super::{View, Event, Hub, Bus, Id, ID_FEEDER, RenderQueue, RenderData, SliderId, THICKNESS_SMALL};
-use crate::color::{BLACK, WHITE, PROGRESS_VALUE, PROGRESS_FULL, PROGRESS_EMPTY};
-use crate::font::{Fonts, font_from_style, SLIDER_VALUE};
-use crate::geom::{Rectangle, BorderSpec, CornerSpec, halves};
+use super::{
+    Bus, Event, Hub, Id, RenderData, RenderQueue, SliderId, View, ID_FEEDER, THICKNESS_SMALL,
+};
+use crate::color::{BLACK, PROGRESS_EMPTY, PROGRESS_FULL, PROGRESS_VALUE, WHITE};
 use crate::context::Context;
+use crate::device::CURRENT_DEVICE;
+use crate::font::{font_from_style, Fonts, SLIDER_VALUE};
+use crate::framebuffer::{Framebuffer, UpdateMode};
+use crate::geom::{halves, BorderSpec, CornerSpec, Rectangle};
+use crate::input::{DeviceEvent, FingerStatus};
+use crate::unit::scale_by_dpi;
 
 const PROGRESS_HEIGHT: f32 = 7.0;
 const BUTTON_DIAMETER: f32 = 46.0;
@@ -24,7 +26,13 @@ pub struct Slider {
 }
 
 impl Slider {
-    pub fn new(rect: Rectangle, slider_id: SliderId, value: f32, min_value: f32, max_value: f32) -> Slider {
+    pub fn new(
+        rect: Rectangle,
+        slider_id: SliderId,
+        value: f32,
+        min_value: f32,
+        max_value: f32,
+    ) -> Slider {
         Slider {
             id: ID_FEEDER.next(),
             rect,
@@ -42,11 +50,12 @@ impl Slider {
         let dpi = CURRENT_DEVICE.dpi;
         let button_diameter = scale_by_dpi(BUTTON_DIAMETER, dpi) as i32;
         let (small_radius, big_radius) = halves(button_diameter);
-        let x_offset = x_hit.max(self.rect.min.x + small_radius)
-                            .min(self.rect.max.x - big_radius);
-        let progress = ((x_offset - self.rect.min.x - small_radius) as f32 /
-                        (self.rect.width() as i32 - button_diameter) as f32)
-                       .clamp(0.0, 1.0);
+        let x_offset = x_hit
+            .max(self.rect.min.x + small_radius)
+            .min(self.rect.max.x - big_radius);
+        let progress = ((x_offset - self.rect.min.x - small_radius) as f32
+            / (self.rect.width() as i32 - button_diameter) as f32)
+            .clamp(0.0, 1.0);
         self.value = self.min_value + progress * (self.max_value - self.min_value);
     }
 
@@ -59,37 +68,48 @@ impl Slider {
 }
 
 impl View for Slider {
-    fn handle_event(&mut self, evt: &Event, _hub: &Hub, bus: &mut Bus, rq: &mut RenderQueue, _context: &mut Context) -> bool {
+    fn handle_event(
+        &mut self,
+        evt: &Event,
+        _hub: &Hub,
+        bus: &mut Bus,
+        rq: &mut RenderQueue,
+        _context: &mut Context,
+    ) -> bool {
         match *evt {
-            Event::Device(DeviceEvent::Finger { status, position, .. }) => {
-                match status {
-                    FingerStatus::Down if self.rect.includes(position) => {
-                        self.active = true;
-                        self.update_value(position.x);
-                        rq.add(RenderData::new(self.id, self.rect, UpdateMode::Gui));
-                        bus.push_back(Event::Slider(self.slider_id, self.value, status));
-                        self.last_x = position.x;
-                        true
-                    },
-                    FingerStatus::Motion if self.active && position.x != self.last_x => {
-                        self.update_value(position.x);
-                        rq.add(RenderData::no_wait(self.id, self.rect, UpdateMode::FastMono));
-                        bus.push_back(Event::Slider(self.slider_id, self.value, status));
-                        self.last_x = position.x;
-                        true
-                    },
-                    FingerStatus::Up if self.active => {
-                        self.active = false;
-                        if position.x != self.last_x {
-                            self.update_value(position.x);
-                            self.last_x = position.x;
-                        }
-                        rq.add(RenderData::new(self.id, self.rect, UpdateMode::Gui));
-                        bus.push_back(Event::Slider(self.slider_id, self.value, status));
-                        true
-                    },
-                    _ => self.active,
+            Event::Device(DeviceEvent::Finger {
+                status, position, ..
+            }) => match status {
+                FingerStatus::Down if self.rect.includes(position) => {
+                    self.active = true;
+                    self.update_value(position.x);
+                    rq.add(RenderData::new(self.id, self.rect, UpdateMode::Gui));
+                    bus.push_back(Event::Slider(self.slider_id, self.value, status));
+                    self.last_x = position.x;
+                    true
                 }
+                FingerStatus::Motion if self.active && position.x != self.last_x => {
+                    self.update_value(position.x);
+                    rq.add(RenderData::no_wait(
+                        self.id,
+                        self.rect,
+                        UpdateMode::FastMono,
+                    ));
+                    bus.push_back(Event::Slider(self.slider_id, self.value, status));
+                    self.last_x = position.x;
+                    true
+                }
+                FingerStatus::Up if self.active => {
+                    self.active = false;
+                    if position.x != self.last_x {
+                        self.update_value(position.x);
+                        self.last_x = position.x;
+                    }
+                    rq.add(RenderData::new(self.id, self.rect, UpdateMode::Gui));
+                    bus.push_back(Event::Slider(self.slider_id, self.value, status));
+                    true
+                }
+                _ => self.active,
             },
             _ => false,
         }
@@ -103,36 +123,58 @@ impl View for Slider {
 
         let progress = (self.value - self.min_value) / (self.max_value - self.min_value);
         let (small_radius, big_radius) = halves(button_diameter);
-        let x_offset = self.rect.min.x + small_radius +
-                       ((self.rect.width() as f32 - button_diameter as f32) * progress) as i32;
+        let x_offset = self.rect.min.x
+            + small_radius
+            + ((self.rect.width() as f32 - button_diameter as f32) * progress) as i32;
 
         fb.draw_rectangle(&self.rect, WHITE);
 
         let (small_mini_radius, big_mini_radius) = halves(progress_height);
         let (small_padding, big_padding) = halves(self.rect.height() as i32 - progress_height);
-        let rect = rect![self.rect.min.x + small_radius - big_mini_radius, self.rect.min.y + small_padding,
-                         self.rect.max.x - big_radius + small_mini_radius, self.rect.max.y - big_padding];
+        let rect = rect![
+            self.rect.min.x + small_radius - big_mini_radius,
+            self.rect.min.y + small_padding,
+            self.rect.max.x - big_radius + small_mini_radius,
+            self.rect.max.y - big_padding
+        ];
 
-        fb.draw_rounded_rectangle_with_border(&rect,
-                                              &CornerSpec::Uniform(small_mini_radius),
-                                              &BorderSpec { thickness: border_thickness,
-                                                            color: BLACK },
-                                              &|x, _| if x < x_offset { PROGRESS_FULL }
-                                                      else { PROGRESS_EMPTY });
+        fb.draw_rounded_rectangle_with_border(
+            &rect,
+            &CornerSpec::Uniform(small_mini_radius),
+            &BorderSpec {
+                thickness: border_thickness,
+                color: BLACK,
+            },
+            &|x, _| {
+                if x < x_offset {
+                    PROGRESS_FULL
+                } else {
+                    PROGRESS_EMPTY
+                }
+            },
+        );
 
         let (small_padding, big_padding) = halves(self.rect.height() as i32 - button_diameter);
-        let rect = rect![x_offset - small_radius, self.rect.min.y + small_padding,
-                         x_offset + big_radius, self.rect.max.y - big_padding];
+        let rect = rect![
+            x_offset - small_radius,
+            self.rect.min.y + small_padding,
+            x_offset + big_radius,
+            self.rect.max.y - big_padding
+        ];
         let fill_color = if self.active { BLACK } else { WHITE };
 
-        fb.draw_rounded_rectangle_with_border(&rect,
-                                              &CornerSpec::Uniform(small_radius),
-                                              &BorderSpec { thickness: 2 * border_thickness,
-                                                            color: BLACK },
-                                              &fill_color);
+        fb.draw_rounded_rectangle_with_border(
+            &rect,
+            &CornerSpec::Uniform(small_radius),
+            &BorderSpec {
+                thickness: 2 * border_thickness,
+                color: BLACK,
+            },
+            &fill_color,
+        );
 
         let font = font_from_style(fonts, &SLIDER_VALUE, dpi);
-        let plan = font.plan(&format!("{:.1}", self.value), None, None);
+        let plan = font.plan(&format!("{:.0}", self.value), None, None);
         let x_height = font.x_heights.1 as i32;
 
         let x_drift = if self.value > (self.min_value + self.max_value) / 2.0 {
@@ -141,7 +183,10 @@ impl View for Slider {
             small_radius
         };
 
-        let pt = pt!(x_offset + x_drift, self.rect.min.y + x_height.max(small_padding));
+        let pt = pt!(
+            x_offset + x_drift,
+            self.rect.min.y + x_height.max(small_padding)
+        );
         font.render(fb, PROGRESS_VALUE, &plan, pt);
     }
 
